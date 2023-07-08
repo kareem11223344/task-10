819122761b9cc08c9beb2afac4489b4b58849496
Karem Ahmed <113023102+kareem11223344@users.noreply.github.com>
2023-07-09 01:00:50 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,51 @@
+// let closeNav = document.querySelector(".close-nav");
+// let nav = document.querySelector(".sub-nav");
+// let menuIcon = document.querySelector(".menu-icon");
+// closeNav.onclick = function(){
+//      nav.style.right="-100%";  
+//      }
+//      menuIcon.onclick = function(){
+//       nav.style.right="0px";  
+//       }    
+// let login = document.querySelector(".login");
+// let registerForm = document.querySelector(".register-form");
+// let closeRegister = document.querySelector(".close-register");
+// login.onclick = function(){
+//   registerForm.style.display="block";
+// }
+// closeRegister.onclick = function(){
+//   registerForm.style.display="none";
+// }
+
+//       $(".search-icon").click(function(){
+//         $(".search-form").slideToggle();
+//       });
+
+// // Set counter default to zero
+// var counter = 0
+
+// // Display total
+// $(".counter").val(counter);
+
+// // When button is clicked
+// $(".add").click(function(){
+//   //Add 10 to counter
+//   counter = counter + 1;
+// 	// Display total
+// 	$(".counter").val(counter);
+// });
+
+
+// //Subtract
+// $(".subtract").click(function(){
+//   counter = counter - 1;
+//   $(".counter").val(counter);
+// });
+
+
+// Reset
+// $("#reset").click(function(){
+//   counter = 0;
+//   $("#counter").text(counter);
+// });
+
